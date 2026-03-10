2c28db1389bdf68c8f4a92fe3a195a50d6d9e277
OlivierSud <olivier.sudermann@gmail.com>
2026-03-10 17:14:25 +0100

    fix: PWA

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,29 +9,6 @@ export default defineConfig({
     port: 5173,
   },
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        name: 'OS List Manager',
-        short_name: 'OS Lists',
-        description: 'Manage your tasks and shopping lists with Google Sheets sync.',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'https://img.icons8.com/color/192/checklist--v1.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'https://img.icons8.com/color/512/checklist--v1.png',
-            sizes: "512x512",
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
+    react()
   ],
 })
