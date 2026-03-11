6a2877fc642020b12362425da246eb898cc8ec53
OlivierSud <olivier.sudermann@gmail.com>
2026-03-11 17:14:41 +0100

    fix: pwa

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'os-list-v33';
+const CACHE_NAME = 'os-list-v34';
 const ASSETS = [
     './',
     './index.html',
     './css/style.css?v=6',
-    './js/app.js?v=33',
+    './js/app.js?v=34',
     './js/supabase-config.js',
     './manifest.json',
     './icone_ListManager.jpg',
